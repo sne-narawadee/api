f9e119b1c116d85561f23fe5021bf1d264bad086
sne-narawadee <166264806+sne-narawadee@users.noreply.github.com>
2025-03-31 16:27:15 +0700

    Update line.js
    "Line test api"

--- line.js
+++ line.js
@@ -1,10 +1,4 @@
-// api/line.js
-export default (req, res) => {
-  // สำหรับ LINE Webhook Verification
-  if (req.method === "GET" || !req.body.events) {
-    return res.status(200).end(); // ต้องตอบ 200 เวลา Verify
-  }
-  // จัดการข้อความจาก LINE
-  console.log(req.body.events);
-  res.json({ replies: [{ type: "text", text: "Hello from LINE!" }] });
-};
+// pages/api/line.js
+export default function handler(req, res) {
+  res.status(200).json({ message: "API is working!" });
+}
